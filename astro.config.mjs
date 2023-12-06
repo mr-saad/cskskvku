681da030b4e35268236e338119b879e1993748cc
saadkhatri1 <khatrisaad233@gmail.com>
2023-12-06 23:57:30 +0530

    demo

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [],
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true
+  },
+  devToolbar: {
+    enabled: false
+  },
+  site: "https://cskskvku.vercel.app"
+})
