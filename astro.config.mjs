795fd45199483ca5b15b2f544886de5a2d139439
saadkhatri1 <khatrisaad233@gmail.com>
2024-01-07 22:07:14 +0530

    major changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+  experimental: {
+    contentCollectionCache: true
+  },
   prefetch: {
     defaultStrategy: "viewport",
     prefetchAll: true
